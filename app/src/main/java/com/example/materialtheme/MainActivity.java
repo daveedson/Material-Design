d8b5a149f044f0c8972f3355f1ce7c042d1d181b
daveedson <daveeddrumz@gmail.com>
2020-02-06 16:03:42 +0100

    Added TextFields to The activitymain.xml

--- app/src/main/java/com/example/materialtheme/MainActivity.java
+++ app/src/main/java/com/example/materialtheme/MainActivity.java
@@ -1,30 +1,58 @@
 package com.example.materialtheme;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
 
+        //initializing of Member variable ToolBar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initialize drawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //initialize navigationView
+        navigationView = findViewById(R.id.Navigation_view);
+
 
         //backButton on action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //  getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Object Creation for ActionBar DrawerToogle.....
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toogle);
+
+        toogle.syncState();
+
+
+        //intializing of NavigationView.setNavigationItemSelectedListener
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -33,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
 
-        getMenuInflater().inflate(R.menu.my_menu,menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -43,16 +71,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
             case R.id.AboutUs_myMenu:
                 Toast.makeText(this, "About Us Clicked", Toast.LENGTH_SHORT).show();
                 break;
 
-            case  R.id.settings_myMenu:
+            case R.id.settings_myMenu:
                 Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
                 break;
-            case  R.id.Users_myMenu:
+            case R.id.Users_myMenu:
                 Toast.makeText(this, "Users clicked", Toast.LENGTH_SHORT).show();
 
             case R.id.search_myMenu:
@@ -62,13 +90,75 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Cart Icon is clicked", Toast.LENGTH_SHORT).show();
 
                 break;
+
+            //Back button to  finish the application.
             case android.R.id.home:
                 finish();
                 break;
         }
 
 
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.Inbox_id:
+                Toast.makeText(MainActivity.this, "Inbox is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Sent_id:
+                Toast.makeText(this, "Sent is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Starred_id:
+                Toast.makeText(this, "Starred is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Draft_id:
+                Toast.makeText(this, "Draft is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.All_Emails:
+                Toast.makeText(this, "All Emails is clicked", Toast.LENGTH_SHORT).show();
+            case R.id.Trash_id:
+                Toast.makeText(this, "Trash is clicked", Toast.LENGTH_SHORT).show();
+            case R.id.Spam_id:
+                Toast.makeText(this, "Spam is clicked", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //Method to close drawer at click of any item withing the drawerLayout
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    //method to use back button to drawer in the Drawerlayout
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else {
+            super.onBackPressed();
+
+        }
+
+
+    }
+
+
+    //Method to hide keyboard when RootLayout is tapped.....
+    public void rootLayoutTapped(View view) {
+
+
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
